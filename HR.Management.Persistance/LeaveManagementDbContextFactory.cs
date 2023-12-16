bc09e52a2426536bc52919627bf3c382fe489e09
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-16 12:16:45 +0500

    design time factory

---
+++ HR.Management.Persistance/LeaveManagementDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistance;
+public class LeaveManagementDbContextFactory : IDesignTimeDbContextFactory<LeaveManagementDbContext>
+{
+    public LeaveManagementDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+        var builder = new DbContextOptionsBuilder<LeaveManagementDbContext>();
+        var connectionString = configuration.GetConnectionString("LeaveManagementConnectionString");
+
+        builder.UseSqlServer(connectionString);
+
+        return new LeaveManagementDbContext(builder.Options);
+    }
+}
