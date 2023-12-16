a76d3737778fe6da29d8f3ba0ebeb9468a2b03a7
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-16 18:54:17 +0500

    entity type configurations for models

---
+++ HR.Management.Persistance/Configurations/Entites/LeaveAllocationConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistance.Configurations.Entites;
+public class LeaveAllocationConfiguration : IEntityTypeConfiguration<LeaveAllocation>
+{
+    public void Configure(EntityTypeBuilder<LeaveAllocation> builder)
+    {
+    }
+}
---
+++ HR.Management.Persistance/Configurations/Entites/LeaveTypeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistance.Configurations.Entites;
+public class LeaveTypeConfiguration : IEntityTypeConfiguration<LeaveType>
+{
+    public void Configure(EntityTypeBuilder<LeaveType> builder)
+    {
+        builder.HasData(
+            new LeaveType
+            {
+                Id = 1,
+                DefaultDays = 10,
+                Name = "Vacation",
+            },
+            new LeaveType
+            {
+                Id = 2,
+                DefaultDays = 12,
+                Name = "Sick"
+            });
+    }
+}
