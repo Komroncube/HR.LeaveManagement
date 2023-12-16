00fb9ef1cc883933a4458018f4076dcaa9f4df51
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-16 19:11:18 +0500

    mvc project added

---
+++ HR.LeaveManagement.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HR.LeaveManagement.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
