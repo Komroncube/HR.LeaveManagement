200854c64621f4414f3c565a098c2a7704953986
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-16 18:54:56 +0500

    avoiding nulls

--- HR.LeaveManagement.Domain/Common/BaseDomainEntity.cs
+++ HR.LeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -6,8 +6,8 @@ namespace HR.LeaveManagement.Domain.Common
     {
         public int Id { get; set; }
         public DateTime DateCreated { get; set; }
-        public string CreatedBy { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
         public DateTime LastModifiedDate { get; set; }
-        public string LastModifiedBy { get; set; }
+        public string LastModifiedBy { get; set; } = string.Empty;
     }
 }
