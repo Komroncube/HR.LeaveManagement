dbf7ac155a2f202216efffc086e2babcb863e2ea
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-17 21:36:02 +0500

    mapper for leaveTypeDtos

--- HR.LeaveManagement.Application/Profiles/MappingProfile.cs
+++ HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -1,7 +1,8 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.DTOs.LeaveAllocation;
 using HR.LeaveManagement.Application.DTOs.LeaveRequest;
-using HR.LeaveManagement.Application.DTOs.LeaveType;
+using HR.LeaveManagement.Application.UseCases.LeaveTypes;
+using HR.LeaveManagement.Application.UseCases.LeaveTypes.Commands.CreateLeaveType;
 using HR.LeaveManagement.Domain;
 
 namespace HR.LeaveManagement.Application.Profiles
@@ -10,10 +11,16 @@ namespace HR.LeaveManagement.Application.Profiles
     {
         public MappingProfile()
         {
+            //LeaveType
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
+
+
+
+
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
             CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
-            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
         }
     }
 }
