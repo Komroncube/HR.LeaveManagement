186d6dbe3f66c8bbb1e293018ed51a3cc267e2b5
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-17 21:38:01 +0500

    dtos and validation methods

---
+++ HR.LeaveManagement.Application/UseCases/LeaveTypes/LeaveTypeDto.cs
@@ -0,0 +1,11 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+using HR.LeaveManagement.Application.DTOs.LeaveType;
+
+namespace HR.LeaveManagement.Application.UseCases.LeaveTypes
+{
+    public class LeaveTypeDto : BaseDto, IBaseLeaveTypeDto
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
---
+++ HR.LeaveManagement.Application/UseCases/LeaveTypes/LeaveTypeDtoValidatorBase.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.DTOs.LeaveType;
+
+namespace HR.LeaveManagement.Application.UseCases.LeaveTypes
+{
+    public class LeaveTypeDtoValidatorBase<T> : AbstractValidator<T> where T : IBaseLeaveTypeDto
+    {
+        public LeaveTypeDtoValidatorBase()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("{PropertyName} is requred.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed {ComparisonValue} characters");
+            RuleFor(p => p.DefaultDays)
+                .NotEmpty().WithMessage("{PropertyName} is requred.")
+                .GreaterThan(0).WithMessage("{PropertyName} must be at least 1")
+                .LessThan(100).WithMessage("{PropertyName} must be less than {ComparisonValue}");
+        }
+    }
+}
