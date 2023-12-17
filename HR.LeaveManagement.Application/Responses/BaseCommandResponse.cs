3c671617431d89ec6b58ad2dd15db4cd2f512863
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-17 21:36:14 +0500

    common response

---
+++ HR.LeaveManagement.Application/Responses/BaseCommandResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace HR.LeaveManagement.Application.Responses;
+public class BaseCommandResponse
+{
+    public int Id { get; set; }
+    public bool IsSuccess { get; set; } = true;
+    public string Message { get; set; }
+    public List<string> Errors { get; set; }
+}
