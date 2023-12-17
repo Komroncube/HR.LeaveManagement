956fc73138cc786e429e70464799604e9d978148
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-17 21:35:45 +0500

    positoion of this dto changed

--- HR.LeaveManagement.Application/DTOs/LeaveType/IBaseLeaveTypeDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HR.LeaveManagement.Application.DTOs.LeaveType
-{
-    public interface IBaseLeaveTypeDto
-    {
-        public string Name { get; set; }
-        public int DefaultDays { get; set; }
-    }
-}
