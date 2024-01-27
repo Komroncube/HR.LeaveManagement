8b2678946bb7348c3f918d5ecdf46dfa6e75800d
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-27 18:34:52 +0500

    null check for leaveallocation

--- HR.LeaveManagement.Application/UseCases/LeaveAllocations/Commands/UpdateLeaveAllocation/UpdateLeaveAllocationCommandHandler.cs
+++ HR.LeaveManagement.Application/UseCases/LeaveAllocations/Commands/UpdateLeaveAllocation/UpdateLeaveAllocationCommandHandler.cs
@@ -4,6 +4,7 @@ using HR.LeaveManagement.Application.Messaging;
 using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
+using HR.LeaveManagement.Application.Exceptions;
 
 namespace HR.LeaveManagement.Application.UseCases.LeaveAllocations.Commands.UpdateLeaveAllocation
 {
@@ -20,7 +21,10 @@ namespace HR.LeaveManagement.Application.UseCases.LeaveAllocations.Commands.Upda
         public async Task<Unit> Handle(UpdateLeaveAllocationCommand request, CancellationToken cancellationToken)
         {
             var leaveAllocation = await _leaveAllocationRepository.GetAsync(request.UpdateLeaveAllocationDto.Id);
-
+            if (leaveAllocation is null)
+            {
+                throw new NotFoundException(nameof(leaveAllocation), request.UpdateLeaveAllocationDto.Id);
+            }
             _mapper.Map(request.UpdateLeaveAllocationDto, leaveAllocation);
 
             await _leaveAllocationRepository.UpdateAsync(leaveAllocation);
