38ff7af808385ace68499dcc9c815b52b1e75a19
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-05-16 21:50:31 +0500

    MINOR FIX: fix spellin error in LeaveRequest.cs

--- HR.LeaveManagement.Domain/LeaveRequest.cs
+++ HR.LeaveManagement.Domain/LeaveRequest.cs
@@ -10,7 +10,7 @@ namespace HR.LeaveManagement.Domain
         public LeaveType LeaveType { get; set; }
         public int LeaveTypeId { get; set; }
         public DateTime DateRequested { get; set; }
-        public string RequestCommnets { get; set; }
+        public string RequestComments { get; set; }
         public DateTime? DateActioned { get; set; }
         public bool? Approved { get; set; }
         public bool Cancelled { get; set; }
