5b1e26d2fac250cb9a2bda804bd35c9714c72146
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-05-20 21:31:26 +0500

    LeaveRequest: Add configurations for LeaveRequest

---
+++ HR.Management.Persistance/Migrations/20240520162745_AddEntityConfigurations.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR.LeaveManagement.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEntityConfigurations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RequestComments",
+                table: "LeaveRequests",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateRequested",
+                table: "LeaveRequests",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 5, 20, 21, 27, 45, 468, DateTimeKind.Local).AddTicks(4886),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RequestComments",
+                table: "LeaveRequests",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateRequested",
+                table: "LeaveRequests",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 5, 20, 21, 27, 45, 468, DateTimeKind.Local).AddTicks(4886));
+        }
+    }
+}
