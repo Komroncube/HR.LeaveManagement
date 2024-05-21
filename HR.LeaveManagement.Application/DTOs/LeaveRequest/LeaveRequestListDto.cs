f02ffefe37704d70f2062e34067d83dbdd19c88b
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-05-21 18:36:53 +0500

    LeaveRequestListDto: rename property

--- HR.LeaveManagement.Application/DTOs/LeaveRequest/LeaveRequestListDto.cs
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/LeaveRequestListDto.cs
@@ -6,7 +6,7 @@ namespace HR.LeaveManagement.Application.DTOs.LeaveRequest
 {
     public class LeaveRequestListDto : BaseDto
     {
-        public LeaveTypeDto LeaveType { get; set; }
+        public LeaveTypeDto LeaveTypeDto { get; set; }
         public DateTime DateRequested { get; set; }
         public bool? Approved { get; set; }
     }
