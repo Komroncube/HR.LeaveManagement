11d2e42633a93f06d5a18e5717f04396fc94ae99
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-05-21 18:35:31 +0500

    ILeaveRequestDtoValidator: Second time validation is not needed because the StartDate is already validated

--- HR.LeaveManagement.Application/DTOs/LeaveRequest/Validators/ILeaveRequestDtoValidator.cs
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/Validators/ILeaveRequestDtoValidator.cs
@@ -1,12 +1,15 @@
 ﻿using FluentValidation;
 using HR.LeaveManagement.Application.Contracts.Persistance;
+using HR.LeaveManagement.Application.Exceptions;
 
 namespace HR.LeaveManagement.Application.DTOs.LeaveRequest.Validators
 {
     public class ILeaveRequestDtoValidator : AbstractValidator<IBaseLeaveRequestDto>
     {
-        public ILeaveRequestDtoValidator(ILeaveTypeRepository leaveTypeRepository) : this()
+        public ILeaveRequestDtoValidator(ILeaveTypeRepository leaveTypeRepository)
         {
+            RuleFor(p => p.StartDate)
+                .LessThan(x => x.EndDate).WithMessage("{PropertyName} must be before {ComparisonValue}");
 
             RuleFor(p => p.LeaveTypeId)
                 .GreaterThan(0)
