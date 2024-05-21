b0f2177236721b482fba14da96d7a780449241d2
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-05-21 17:39:12 +0500

    GetLeaveRequestListQuery/Handler: fixing namespace spelling

--- HR.LeaveManagement.Application/UseCases/LeaveRequests/Queries/GetLeaveReqeustList/GetLeaveRequestListQuery.cs
+++ HR.LeaveManagement.Application/UseCases/LeaveRequests/Queries/GetLeaveReqeustList/GetLeaveRequestListQuery.cs
@@ -2,7 +2,7 @@
 using HR.LeaveManagement.Application.Messaging;
 using System.Collections.Generic;
 
-namespace HR.LeaveManagement.Application.UseCases.LeaveRequests.Queries.GetLeaveReqeustList
+namespace HR.LeaveManagement.Application.UseCases.LeaveRequests.Queries.GetLeaveRequestList
 {
     public class GetLeaveRequestListQuery : IQuery<List<LeaveRequestListDto>>
     {
